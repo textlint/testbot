72a7fe0a1f0f55433d9d7bbb3059dc86d928d14a
azu <azuciao@gmail.com>
2016-06-09 00:07:06 +0900

    test: add docs.userdive.com

--- test.sh
+++ test.sh
@@ -4,3 +4,4 @@ set -eu
 ./run_lint gitbook-starter-kit ja/
 ./run_lint scala_text src/
 ./run_lint magi-hacker "chapter-01/ chapter-02/ chapter-03/ chapter-04/ chapter-05/"
+./run_lint docs.userdive.com ja/
