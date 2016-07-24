5dc49eb85fbf67ef811bd3e934ce2007a596f805
azu <azuciao@gmail.com>
2016-07-24 22:36:31 +0900

    remove unused project

--- test.sh
+++ test.sh
@@ -4,4 +4,3 @@ set -eu
 ./run_lint gitbook-starter-kit ja/
 ./run_lint scala_text src/
 ./run_lint magi-hacker "chapter-01/ chapter-02/ chapter-03/ chapter-04/ chapter-05/"
-./run_lint aws-short-handbook "README.md SUMMARY.md tldr"
